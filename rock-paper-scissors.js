5678a4bcd0160b05ea9c8adbf983862437c34d59
Tamara <tmwoodson@gmail.com>
2015-12-20 20:03:25 -0800

    Make code more DRY

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,30 +1,87 @@
 Players = new Mongo.Collection('players');
+Rounds = new Mongo.Collection('rounds');
+
+var getOpponentNumber = function(playerNumber) {
+  if (playerNumber === 1) {
+    return 2;
+  }
+  return 1;
+};
+
+//Utility function to compute whether player can submit a choice.
+var hasTurn = function(playerNumber, id) {
+  var opponentNumber = getOpponentNumber();
+
+  var opponent = Players.find({'player': opponentNumber}, {limit: 1});
+  if (opponent.count() === 0) {
+    return false;
+  }
+  var opponentStats = opponent.fetch()[0];
+  var myStats = Players.find({ '_id': id }).fetch()[0];
+
+  console.log('using hasTurn. OpponentStats:', opponentStats);
+  console.log('using hasTurn. MyStats:', myStats);
+  return (opponentStats.turn === myStats.turn) || (myStats.turn < opponentStats.turn);    
+};
+
+//Utility function to return the hash of template helpers for each player
+var getHelpers = function(playerNumber, session) {
+  var opponentNumber = getOpponentNumber();
+ 
+  return {
+    myData: function() {
+      return Players.find({ '_id': session.get('id') });
+    },
+
+    opponentData: function() {
+      return Players.find({ 'player': opponentNumber }, {limit: 1});
+    },
+
+    opponentMissing: function() {
+      return Players.find({'player': opponentNumber}).count() === 0;
+    },
+
+    hasTurn: function() {
+      return hasTurn(1, session.get('id'));
+    }
+  };
+};
+
+var init = function(playerNumber, session) {
+    var opponentNumber = getOpponentNumber();
+
+    var player = Players.find({'player': playerNumber}).fetch();
+    var id;
+    if (player.length) {
+      id = player[0]._id;
+      console.log('player already exists; using old id:', id);
+    } else {
+      id = Players.insert({'player': playerNumber, 'turn': 0, 'score': 0});
+      console.log('player did not exist; creating new record:', id);
+    }
+    session.set('id', id);
+    session.set('previousResult', {});
+    
+    var opponent = Players.find({'player': opponentNumber});
+    var handle = opponent.observeChanges({
+      added: function(id, fields) {
+        console.log('player added:', id, fields);
+      },
+      changed: function(id, fields) {
+        console.log('player changed:', id, fields);
+      }
+    });
+};
 
 if (Meteor.isClient) {
 
-  Template.player1.helpers({
-    myStats: function() {
-      return Players.find({ 'player': 1 });
-    },
+  Template.player1.helpers(getHelpers(1, Session));
 
-    opponentStats: function() {
-      return Players.find({ 'player': 2 });
-    }
-  });
-
-  Template.player2.helpers({
-    myStats: function() {
-      return Players.find({ 'player': 2 });
-    },
-
-    opponentStats: function() {
-      return Players.find({ 'player': 1 });
-    }
-  });
+  Template.player2.helpers(getHelpers(2, Session));
 
   Template.player1.events({
-    'click button': function () {
-      console.log('what are my stats?', Players.find({}));
+    'click button': function (e) {
+      console.log('what is target?', $(e.target).hasClass('rock'));
     }
   });
 
@@ -35,23 +92,14 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.player1.onRendered(function() {
-    var player = Players.find({'player': 1}).fetch();
-    var id;
-    if (player.length) {
-      // player already exists
-      console.log('Here are my stats:', player[0]);
-      id = player[0]._id;
-    } else {
-      id = Players.insert({'player': 1, 'turn': 0, 'score': 0});
-    }
-    Session.set('id', id);
+  Template.player1.onCreated(function() {
+    init(1, Session);
   });
 
-  Template.player2.onRendered(function() {
-    var playerId = Players.insert({'player': 2, 'turn': 0, 'score': 0});
-    Session.set('id', playerId);
+  Template.player2.onCreated(function() {
+    init(2, Session);
   });
+
 }
 
 if (Meteor.isServer) {
